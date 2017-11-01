6248189fd9d3d990f04447bdba5e824cb0a93f46
Krishna Pakalapati <Krishna@Krishnas-MacBook-Pro.local>
2017-10-31 19:08:58 -0700

    added extra info, made it look like example

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 vector<Puzzle*> visited;
+int max_queue = 0;
 
 bool unique(Node* n)
 {
@@ -133,16 +134,22 @@ Node* search(Node* root, int choice)
         return root;
     }
     visited.push_back(root->get_p());
-    
     vector<Node*> kyu;
     add_kids(root, kyu);
-    cout << "root has " << kyu.size() << " children" << endl;
-    
+    max_queue = kyu.size();
+    //cout << "root has " << kyu.size() << " children" << endl;
+    cout << "Expanding state" << endl;
+    root->get_p()->print();
+    cout << endl;
     bool goal = false;
     Node* goal_node = 0;
     
     while(kyu.size() > 0 && !goal)
     {
+	if(max_queue < kyu.size())
+	{
+		max_queue = kyu.size();
+	}
         Node* curr = 0;
         if(choice == 1)
         {
@@ -164,7 +171,15 @@ Node* search(Node* root, int choice)
         }
         
         cout << endl;
-        cout << curr->get_g() << endl;
+        cout << "The best state to expand with a g(n) = " << curr->get_g() << endl;
+	if(choice == 2)
+	{
+		cout << " and h(n) = " << curr->get_p()->misplaced_tiles() << endl;
+	}
+	else if(choice == 3)
+	{
+		cout << " and h(n) = " << curr->get_p()->manhattan_distance() << endl;
+	}
         curr->get_p()->print();
         cout << endl;
         
@@ -206,36 +221,20 @@ int main()
     cin >> search_choice;
     
     Node* m = 0;
-    if(search_choice == 1)
-    {
-        cout << "You've chosen Uniform Cost Search" << endl;
-        m = search(n, search_choice);
-    }
-    else if(search_choice == 2)
-    {
-        cout << "You've chosen A* Search with the Misplaced Tile Heuristic" << endl;
-        m = search(n, search_choice);
-    }
-    else if(search_choice == 3)
-    {
-        cout << "You've chosen A* Search with the Manhattan Distance Heuristic" << endl;
-        m = search(n, search_choice);
-    }
-    else
-    {
-        cout << "your choice is bad and the rest won't work" << endl;
-    }
+    m = search(n, search_choice);
     
     if(m != 0)
     {
+	cout << "Goal!!" << endl;
+	cout << "To solve this problem the search algorithm expanded a total of " << visited.size() << " nodes." << endl;
+	cout << "The maximum number of nodes in the queue at any one time was " << max_queue << "." << endl;
+	cout << "The depth of the goal node was " << m->get_g() << "." << endl;
         vector<Node*> trace;
         traceback(m, trace);
         
-        cout << trace.size() << endl;
-        cout << "the solution is" << endl;
         cout << endl;
         
-        if(search_choice == 1)
+        /*if(search_choice == 1)
         {
             for(int i = trace.size() - 1; i > -1; i--)
             {
@@ -261,7 +260,8 @@ int main()
                 cout << "current manhattan_distance: "<< trace.at(i)->get_p()->manhattan_distance()<< endl;
                 cout << endl;
             }
-        }
+        }*/
+	
     }
     else
     {
